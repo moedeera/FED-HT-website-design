0ef8248622d1f2ce0102875c53597387c1011a4f
Moe Deera <deeramoe@gmail.com>
2022-08-13 03:19:15 -0600

    committ

--- main.js
+++ main.js
@@ -124,7 +124,7 @@ rtn4.addEventListener("click", (e) => {
     mm = String(today.getMonth() + 1).padStart(2, "0"),
     yyyy = today.getFullYear(),
     nextYear = yyyy + 1,
-    dayMonth = "08/30/",
+    dayMonth = "10/30/",
     birthday = dayMonth + yyyy;
 
   today = mm + "/" + dd + "/" + yyyy;
